7fdef404424b6f0e64d4c89d78024e250764829b
collins njau <collinsnjau39@gmail.com>
2019-07-23 10:14:37 +0300

    Add error handling - add handler for all errors on the application - add morgan for logging requests

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
+app.use(morgan("dev"));
 const smsRoutes = require("./api/routes/sms");
 const contactsRoutes = require("./api/routes/contacts");
+
 /**
  * setup middleware
  * incoming request will
@@ -10,5 +13,20 @@ const contactsRoutes = require("./api/routes/contacts");
  */
 app.use("/api/v1/sms", smsRoutes);
 app.use("/api/v1/contacts", contactsRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
