ee5f04a05370a5ba05b63ed6eaebdb6a56534bde
collins njau <collinsnjau39@gmail.com>
2019-08-05 15:08:40 +0300

    ft: complete sms management - add methods to edit and delete sms

--- api/routes/sms.js
+++ api/routes/sms.js
@@ -2,30 +2,72 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const SmsModel = require("../models/sms");
+const Contact = require("../models/contacts");
 /**
  * register the routes
  */
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "sms get request"
-  });
+  SmsModel.find()
+    .select("message receiverContactId status senderContactId")
+    .exec()
+    .then(messages => {
+      res.status(200).json({
+        count: messages.length,
+        sms: messages.map(message => {
+          return {
+            _id: message._id,
+            sender: message.senderContactId,
+            receiver: message.receiverContactId,
+            status: message.status,
+            message: message.message,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/api/v1/sms/" + message._id
+            }
+          };
+        })
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
-  const sms = new SmsModel({
-    _id: mongoose.Types.ObjectId(),
-    message: req.body.message,
-    receiverContactId: req.body.receiverContactId,
-    status: req.body.status,
-    senderContactId: req.body.senderContactId
-  });
-  sms
-    .save()
+  Contact.findById(req.body.receiverContactId)
+    .then(contact => {
+      if (!contact) {
+        return res.status(404).json({
+          message: "The contact does not exist"
+        });
+      }
+
+      const sms = new SmsModel({
+        _id: mongoose.Types.ObjectId(),
+        message: req.body.message,
+        receiverContactId: req.body.receiverContactId,
+        status: req.body.status,
+        senderContactId: req.body.senderContactId
+      });
+      return sms.save();
+    })
     .then(result => {
-      console.log(result);
       res.status(200).json({
         message: "SMS was created",
-        smsCreated: result
+        smsCreated: {
+          _id: result._id,
+          sender: result.senderContactId,
+          receiver: result.receiverContactId,
+          status: result.status,
+          message: result.message
+        },
+        request: {
+          type: "GET",
+          description: "Get a single sms",
+          url: "http://localhost:3000/api/v1/sms/" + result._id
+        }
       });
     })
     .catch(error => {
@@ -36,17 +78,29 @@ router.post("/", (req, res, next) => {
 });
 
 router.get("/:smsId", (req, res, next) => {
-  const { smsId } = req.params;
-  if (smsId === "special") {
-    res.status(200).json({
-      message: "You are special",
-      id: smsId
+  const smsId = req.params.smsId;
+  SmsModel.findById(smsId)
+    .exec()
+    .then(result => {
+      if (!result) {
+        return res.status(404).json({
+          message: "The sms does not exist"
+        });
+      }
+      res.status(200).json({
+        sms: result,
+        response: {
+          type: "GET",
+          description: "Get all sms",
+          url: "http://localhost:3000/api/v1/sms/"
+        }
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
     });
-  } else {
-    res.status(200).json({
-      message: "You are not special"
-    });
-  }
 });
 
 router.patch("/:smsId", (req, res, next) => {
@@ -56,9 +110,30 @@ router.patch("/:smsId", (req, res, next) => {
 });
 
 router.delete("/:smsId", (req, res, next) => {
-  res.status(200).json({
-    message: "Delete product"
-  });
+  const smsId = req.params.smsId;
+  SmsModel.remove({ _id: smsId })
+    .exec()
+    .then(result => {
+      console.log(result.n);
+      if (result.n == 0) {
+        return res.status(404).json({
+          message: "The sms does not exist"
+        });
+      }
+      res.status(200).json({
+        message: "SMS was deleted",
+        request: {
+          type: "GET",
+          description: "Get all sms",
+          url: "http://localhost:3000/api/v1/sms/"
+        }
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 module.exports = router;
